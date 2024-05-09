fa17f400c09d630315b3d644ea3f98d0c3263cc8
AhmetRecaiElcan <ahmetrecaiellcan@gamail.com>
2024-05-09 18:35:54 +0300

    ayağa kaldırma

---
+++ lib/screens/flight/category_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:bruh/provider/app_state.dart';
+import 'package:bruh/utils/colors_const.dart';
+import 'flight_type.dart';
+
+class CategoryWidget extends StatelessWidget {
+  final Category category;
+  const CategoryWidget({Key? key, required this.category}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final appState = Provider.of<AppState>(context);
+    final isSelected = appState.selectedCategory == category.categoryId;
+
+    return GestureDetector(
+      onTap: () {
+        if (!isSelected) {
+          appState.updateCategory(category.categoryId);
+        }
+      },
+      child: Container(
+        width: 115,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.white : Colors.transparent,
+          borderRadius: BorderRadius.circular(18.0),
+        ),
+        child: Center(
+          child: Text(
+            category.name,
+            style: isSelected
+                ? TextStyle(color: ColorsConstants.appColor)
+                : const TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/profile/privacy_policy.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PrivacyPolicyScreen extends StatelessWidget {
+  const PrivacyPolicyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Privacy Policy'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            _launchURL('https://www.google.com/');
+          },
+          child: Text('Open Google'),
+        ),
+      ),
+    );
+  }
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
